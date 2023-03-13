9a21d8e2eeeef3dbe2d1831ccb96992216022d23
febronia53 <febroniasameh5@gmail.com>
2023-03-13 23:12:59 +0200

    finish design

--- app/src/main/java/com/example/programmercalculator/MainActivity.kt
+++ app/src/main/java/com/example/programmercalculator/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.programmercalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
